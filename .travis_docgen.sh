9cfe19de5799fcc43cc9afdf445014f89716a0d6
a.wasiak@sare.pl <a.wasiak@sare.pl>
2017-03-06 15:15:28 +0100

    init

---
+++ .travis_docgen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+REPOSITORY_ID='SAREhub/php_docker_worker_component'
+
+if [ "$TRAVIS_REPO_SLUG" == "$REPOSITORY_ID" ] && [ $TRAVIS_BRANCH = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_PHP_VERSION" == "7.0" ]; then
+    wget http://www.apigen.org/apigen.phar
+    php apigen.phar generate -s src -d ../gh-pages --template-theme bootstrap
+    cd ../gh-pages
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "Travis"
+    git init
+    git remote add origin https://${GH_TOKEN}@github.com/"$REPOSITORY_ID".git > /dev/null
+    git checkout -B gh-pages
+
+    git add .
+    git commit -m "APIGEN (Travis Build : $TRAVIS_BUILD_NUMBER  - Branch : $TRAVIS_BRANCH)"
+    git push origin gh-pages -fq > /dev/null
+fi
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# php_docker_worker_component
+# SAREhub PHP Docker Commons
+[![Coding Standards](https://img.shields.io/badge/cs-PSR--2--R-yellow.svg)](https://github.com/php-fig-rectified/fig-rectified-standards) [![Build Status](https://travis-ci.org/SAREhub/PHP_Commons.svg?branch=master)](https://travis-ci.org/SAREhub/php_docker_commons) 
+* This project contains common stuff for create docker based application in php
+* [PHPDOC](https://sarehub.github.io/php_docker_commons)
+
+### How do I get set up? ###
+```
+composer require sarehub/docker_commons
+```
