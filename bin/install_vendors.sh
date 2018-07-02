6d0e08e8e9fd730f142c8b90eaeacfdcf27ba49f
jcala <j.cala@sarehub.com>
2018-07-02 13:07:14 +0200

    install vendors script

--- bin/install_vendors.sh
+++ bin/install_vendors.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-DEPLOY_USER=$1
 
 docker run \
   --rm \
   --interactive --tty \
   --volume $PWD:/app \
-  --user $(id -u ${DEPLOY_USER}):$(id -g ${DEPLOY_USER}) \
+  --user $(id -u $DEPLOY_USER):$(id -g $DEPLOY_USER) \
   composer install --no-dev --optimize-autoloader --prefer-dist --no-suggest --ignore-platform-reqs
